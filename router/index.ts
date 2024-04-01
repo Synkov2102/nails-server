df05299ddd3039891764e7f15611b9e755d37936
Eugene <synkoveugeny@gmail.com>
2024-04-01 21:07:37 +0300

    feat: шаг в тесты

--- router/index.ts
+++ router/index.ts
@@ -5,11 +5,15 @@ import authMiddleware from "../middelwares/auth-middleware"
 
 const router = Router()
 
+//authorization
 router.post("/registration",
     body("email").isEmail(),
     body("password").isLength({ min: 3, max: 32 }),
     userController.registration)
-router.post("/login", userController.login)
+router.post("/login",
+    body("email").isEmail(),
+    body("password").isLength({ min: 3, max: 32 }),
+    userController.login)
 router.post("/logout", userController.logout)
 router.get("/activate/:link", userController.activate)
 router.get("/refresh", userController.refresh)
