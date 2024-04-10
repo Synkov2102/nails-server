3999d35fbed44b29fae2291e1b9375ee59042739
Eugene <synkoveugeny@gmail.com>
2024-04-10 15:20:40 +0300

    feat: тесты для авторизации

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,19 +1,29 @@
 import request from "supertest"
 import { app } from "../index"
+import userModel from "../models/user-model"
+import tokenModel from "../models/token-model"
+
+const testUser = {
+    email: "test@gmail.com",
+    password: "1234567890"
+}
+const jwtRegexp = /(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)/
 
 describe("POST /api/registration", () => {
     describe("введена почта и пароль", () => {
         test("должен вернуть код 200", async () => {
-            const response = await request(app).post("/api/registration").send({
-                email: "test@gmail.com",
-                password: "1234567890"
-            })
+
+
+
+            const user = await userModel.findOneAndDelete({ email: testUser.email })
+            if (user) await tokenModel.deleteOne({ user: user._id })
+
+            const response = await request(app).post("/api/registration").send(testUser)
+
+
             expect(response.statusCode).toBe(200)
+            expect(response.body.accessToken).toMatch(jwtRegexp)
+            expect(response.body.refreshToken).toMatch(jwtRegexp)
         })
     })
-
-    describe("введена почта и пароль", () => {
-
-    })
-
 })
